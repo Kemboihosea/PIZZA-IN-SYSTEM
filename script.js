45718252818fb4fe193aff4ee2bf5c92f4b9be64
kemboihosea <kemboihosea98@gmail.com>
2022-01-18 11:45:52 +0300

    code change

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
-// Business logic
-console.log('HI')
-// form submission function
-function getSize() {
-  var size = document.getElementById("pizzaSize").value;
-  return parseInt(size);
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 }
 
-function getCrust() {
-  var crust = document.getElementById("pizzaCrust").value;
-  return parseInt(crust);
-}
+// proceed button
+$(document).ready(function
+  $("button.proceed").click(function(event){
+   let pname = $(".name option:selected").val();
+   let psize = $("#size option:selected").val();
+   let pcrust = $("#crust option:selected").val();
+   let ptopping = [];
+   $.each($("input[name='toppings']:checked"), function(){            
+       ptopping.push($(this).val());
+   });
+   console.log(ptopping.join(", "));
 
-function getTopping() {
-  var topping = document.getElementById("pizzaTopping").value;
-  return parseInt(topping);
-}
+   switch(size);
+    case "0":
+      price =0;
+    break;
+    case "large":
+       price = 1200;
+       console.log(price);
+     break;
+     case "medium":
+       price = 850;
+       console.log("The price is "+price);
+     break;
+     case "small":
+       price = 600;
+       console.log(price);
+     default:
+       console.log("error"); 
+   }
+   switch(pcrust){
+      case "0":
+        crust_price = 0;
+      break;
+      case "Crispy":
+        crust_price = 200;
+      break;
+      case "Stuffed":
+        crust_price = 250;
+      break;
+      case "Gluten-free":
+        crust_price = 180;
+      break;
+      default:
+        console.log("No price"); 
+    }
+    let topping_value = ptopping.length*50;
+    console.log("toppins value" + topping_value);
 
-function getCheese() {
-  var cheese = document.getElementById("pizzaCheese").value;
-  return parseInt(cheese);
-}
+    if((psize == "0") && (pcrust == "0")){
+      console.log("nothing selected");
+      $("button.proceed").show();
+      $("#information").show();
+      $("div.choise").hide();
+      alert("Please select pizza size and crust"); 
+    }
+    else{
+      $("button.proceed").hide();
+      $("#information").hide();
+      $("div.choise").slideDown(1000);
+    }
 
-function getQuantity() {
-  var number = document.getElementById("quantity").value;
-  return parseInt(number);
-}
-
-/* function to calculate the total amount */
-function totalAmount() {
-  var totals = (getSize() + getCrust() + getTopping() + getCheese()) * getQuantity();
-  alert(
-    "You have Ordered " +
-    getQuantity("") +
-    " pizza," + 
-    "" +
-    " amounting to ksh. " +
-    totals +
-    "" +
-    " Thanks for your order welcome again."
-  );
-
-  // variable to get the location of the customer
-  var location = prompt("If you want your pizza to be delivered to you please enter your location and if not cancel.");
-  if (location !== "") {
-    alert("You order will be delivered shortly.Delivery fee is 100/= and incase you didn,t want it delivered please come for your order.");
-  }
-}
+    total = price + crust_price + topping_value;
+    console.log(total);
+    let checkoutTotal =0;
+    checkoutTotal = checkoutTotal + total;
